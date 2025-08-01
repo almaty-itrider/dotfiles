242dd468961a373b5f1e8ffdf68b88ebbb8e2587
Abylay Kuvatov <abukuvatov@icloud.com>
2025-08-01 17:14:07 +0500

    Added zsh aliases and updated NeoVim packages

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,9 @@ alias venv="source .venv/bin/activate"
 alias n="nvim"
 alias open="explorer.exe"
 alias pbcopy="win32yank.exe -i"
+
+# === Docker aliases ===
+alias dc="docker compose"
+alias dcup="docker compose up"
+alias dcd="docker compose down"
+alias dcupd="docker compose up -d"
